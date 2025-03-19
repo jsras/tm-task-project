c11b5716868ed4a4fe74745a6c5043b42eb2efef
jonas <jonas.jsr.rasmussen@gmail.com>
2025-03-19 08:50:58 +0100

    Initial Commit

---
+++ Trackman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trackman
+//
+//  Created by Jonas Rasmussen on 19/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
